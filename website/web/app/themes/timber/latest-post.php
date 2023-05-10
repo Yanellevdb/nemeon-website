055b24243f4acd91de0c8a5192eb5b78e39914ed
Jade Apers <jadeapers@telenet.be>
2023-05-10 15:16:55 +0200

    feat: added content

--- website/web/app/themes/timber/latest-post.php
+++ website/web/app/themes/timber/latest-post.php
@@ -14,25 +14,20 @@ $templates = array('latest-post.twig');
 
 $context = Timber::context();
 
-// Retrieve the latest blog post
-$args = array(
-  'post_type' => 'post',
-  'post_status' => 'publish',
+$latest_post = new Timber\PostQuery([
+  'post_type'      => 'post',
+  'post_status'    => 'publish',
   'posts_per_page' => 1,
-  'orderby' => 'date',
-  'order' => 'DESC',
-);
+  'orderby'        => 'date',
+  'order'          => 'DESC',
+]);
 
-$latest_post = new Timber\PostQuery($args);
+$context['post'] = $latest_post[0];
 
-// Pass the necessary information to the Twig template
-if ($latest_post) {
-  $context['post'] = $latest_post[0];
-  $context['title'] = $latest_post[0]->title;
-  $context['categories'] = $latest_post[0]->categories();
-  $context['tags'] = $latest_post[0]->tags();
-  $context['intro'] = $latest_post[0]->get_field('intro');
-  $context['image'] = $latest_post[0]->get_field('image');
-}
-
-Timber::render($templates, $context);
+$context['block']['title'] = 'Laatste blogpost';
+$context['block']['image'] = $latest_post[0]->image();
+$context['block']['link'] = [
+  'label' => 'Lees meer',
+  'url'   => $latest_post[0]->link(),
+  'text'  => $latest_post[0]->title(),
+];
