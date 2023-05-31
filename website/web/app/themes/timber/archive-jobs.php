e06476e64db5f678989d15ad28ef24f4bf66eac5
Yanellevdb <yanelle1@hotmail.com>
2023-05-31 17:16:57 +0200

    feat: trial jobs

--- website/web/app/themes/timber/archive-jobs.php
+++ website/web/app/themes/timber/archive-jobs.php
@@ -19,20 +19,20 @@ $templates = array( 'archive.twig', 'index.twig' );
 $context = Timber::context();
 
 $context['title'] = 'Archive';
-if ( is_day() ) {
-	$context['title'] = 'Archive: ' . get_the_date( 'D M Y' );
-} elseif ( is_month() ) {
-	$context['title'] = 'Archive: ' . get_the_date( 'M Y' );
-} elseif ( is_year() ) {
-	$context['title'] = 'Archive: ' . get_the_date( 'Y' );
-} elseif ( is_tag() ) {
-	$context['title'] = single_tag_title( '', false );
-} elseif ( is_category() ) {
-	$context['title'] = single_cat_title( '', false );
-	array_unshift( $templates, 'archive-' . get_query_var( 'cat' ) . '.twig' );
-} elseif ( is_post_type_archive() ) {
-	$context['title'] = post_type_archive_title( '', false );
-	array_unshift( $templates, 'archive-' . get_post_type() . '.twig' );
+if (is_day()) {
+    $context['title'] = 'Archive: ' . get_the_date('D M Y');
+} elseif (is_month()) {
+    $context['title'] = 'Archive: ' . get_the_date('M Y');
+} elseif (is_year()) {
+    $context['title'] = 'Archive: ' . get_the_date('Y');
+} elseif (is_tag()) {
+    $context['title'] = single_tag_title('', false);
+} elseif (is_category()) {
+    $context['title'] = single_cat_title('', false);
+    array_unshift($templates, 'archive-' . get_query_var('cat') . '.twig');
+} elseif (is_post_type_archive()) {
+    $context['title'] = post_type_archive_title('', false);
+    array_unshift($templates, 'archive-' . get_post_type() . '.twig');
 }
 
 $context['posts'] = new Timber\PostQuery();
@@ -41,4 +41,8 @@ $context['post_count'] = $wp_query->found_posts;
 $context['jobs_title'] = get_field('title', 'jobs_page');
 $context['jobs_cta'] = get_field('call_to_action', 'jobs_page');
 
-Timber::render( $templates, $context );
+if (is_post_type_archive('jobs')) {
+    $context['jobs_steps'] = get_field('steps', 'options');
+    $context['jobs_banner'] = get_field('full_width_banner', 'options');
+}
+Timber::render($templates, $context);
